41adda62a5916e939d13e8ce5f1e0fe9432ab661
Luciano Junior <lucianomoraisjr@gmail.com>
2022-07-15 09:57:20 -0300

    change SUT creation strategy to use beforeEach

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -3,29 +3,22 @@ import { FacebookAuthenticationService } from '@/data/services'
 import { AuthenticationError } from '@/domain/errors'
 import { mock, MockProxy } from 'jest-mock-extended'
 
-type SutTypes = {
-  sut: FacebookAuthenticationService
-  laodFacebookApi: MockProxy<LoadFacebookUserApi>
-}
-const makeSut = (): SutTypes => {
-  const laodFacebookApi = mock<LoadFacebookUserApi>()
-  const sut = new FacebookAuthenticationService(laodFacebookApi)
-  return {
-    sut,
-    laodFacebookApi
-  }
-}
-
 describe('FacebookAuthenticationService', () => {
+  let sut: FacebookAuthenticationService
+  let laodFacebookApi: MockProxy<LoadFacebookUserApi>
+
+  beforeEach(() => {
+    laodFacebookApi = mock()
+    sut = new FacebookAuthenticationService(laodFacebookApi)
+  })
+
   it('should call LoadFacebookUserApi with correct params', async () => {
-    const { laodFacebookApi, sut } = makeSut()
     await sut.perform({ token: 'any_token' })
     expect(laodFacebookApi.loadUser).toHaveBeenCalledWith({ token: 'any_token' })
     expect(laodFacebookApi.loadUser).toHaveBeenCalledTimes(1)
   })
 
   it('should return AuthenticationError when LoadFacebookApi retunr  undefiened', async () => {
-    const { laodFacebookApi, sut } = makeSut()
     laodFacebookApi.loadUser.mockResolvedValueOnce(undefined)
     const authResult = await sut.perform({ token: 'any_token' })
     expect(authResult).toEqual(new AuthenticationError())
