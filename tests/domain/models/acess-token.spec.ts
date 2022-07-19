8a341a5408482900c9206da8ffa3332dfca6107e
Luciano Junior <lucianomoraisjr@gmail.com>
2022-07-19 15:47:56 -0300

    ensure AccessToken in 30 minutes

--- tests/domain/models/acess-token.spec.ts
+++ tests/domain/models/acess-token.spec.ts
@@ -5,4 +5,7 @@ describe('AccessToken', () => {
     const sut = new AccessToken('any_value')
     expect(sut).toEqual({ value: 'any_value' })
   })
+  it('should expire in 1800000 ms', () => {
+    expect(AccessToken.expirationInMs).toBe(1800000)
+  })
 })
