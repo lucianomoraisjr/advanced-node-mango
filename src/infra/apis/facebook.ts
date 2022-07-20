c964dca4130471a80fbc4aaa35d99c6cae86851f
Luciano Junior <lucianomoraisjr@gmail.com>
2022-07-20 16:23:18 -0300

    ensure facebook return

--- src/infra/apis/facebook.ts
+++ src/infra/apis/facebook.ts
@@ -1,7 +1,7 @@
 import { LoadFacebookUserApi } from '@/data/contracts/apis'
 import { HttpGetClient } from '../http'
 
-export class FacebookApi {
+export class FacebookApi implements LoadFacebookUserApi {
   private readonly baseUrl = 'https://graph.facebook.com'
   constructor (
     private readonly httpClient: HttpGetClient,
@@ -9,7 +9,7 @@ export class FacebookApi {
     private readonly clientSecret: string
   ) {}
 
-  async loadUser (params: LoadFacebookUserApi.Params): Promise<void> {
+  async loadUser (params: LoadFacebookUserApi.Params): Promise<LoadFacebookUserApi.Resut> {
     const appToken = await this.httpClient.get({
       url: `${this.baseUrl}/oauth/access_token`,
       params: {
@@ -25,7 +25,7 @@ export class FacebookApi {
         input_token: params.token
       }
     })
-    await this.httpClient.get({
+    const userinfo = await this.httpClient.get({
       // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
       url: `${this.baseUrl}/${debugToken.data.user_id}`,
       params: {
@@ -33,5 +33,10 @@ export class FacebookApi {
         access_token: params.token
       }
     })
+    return {
+      facebookId: userinfo.id,
+      name: userinfo.name,
+      email: userinfo.email
+    }
   }
 }
--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -17,6 +17,11 @@ describe('FacebookApi', () => {
     hhtpClient.get
       .mockResolvedValueOnce({ access_token: 'any_app_token' })
       .mockResolvedValueOnce({ data: { user_id: 'any_user_id' } })
+      .mockResolvedValueOnce({
+        id: 'any_fb_id',
+        name: 'any_fb_name',
+        email: 'any_fb_email'
+      })
     sut = new FacebookApi(hhtpClient, clientId, clientSecret)
   })
   it('should get app token', async () => {
@@ -51,6 +56,15 @@ describe('FacebookApi', () => {
         fields: 'id,name,email',
         access_token: 'any_client_token'
       }
+    })
+  })
+  it('should return facebook user', async () => {
+    const fbUser = await sut.loadUser({ token: 'any_client_token' })
+
+    expect(fbUser).toEqual({
+      facebookId: 'any_fb_id',
+      name: 'any_fb_name',
+      email: 'any_fb_email'
     })
   })
 })
