04743fdd4fbaa587576dff7d830c34974fdbb35c
Luciano Junior <lucianomoraisjr@gmail.com>
2022-07-20 10:36:37 -0300

    ensure FacebookApi gets debug token

--- src/infra/http/client.ts
+++ src/infra/http/client.ts
@@ -1,9 +1,11 @@
 export interface HttpGetClient {
-  get: (params: HttpGetClient.Params) => Promise<void>
+  get: (params: HttpGetClient.Params) => Promise<HttpGetClient.Result>
 }
 export namespace HttpGetClient {
   export type Params = {
     url: string
     params: object
   }
+
+  export type Result = any
 }
--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -14,6 +14,7 @@ describe('FacebookApi', () => {
     hhtpClient = mock()
   })
   beforeEach(() => {
+    hhtpClient.get.mockResolvedValueOnce({ access_token: 'any_app_token' })
     sut = new FacebookApi(hhtpClient, clientId, clientSecret)
   })
   it('should get app token', async () => {
@@ -25,6 +26,17 @@ describe('FacebookApi', () => {
         client_id: clientId,
         client_secret: clientSecret,
         grant_type: 'client_credentials'
+      }
+    })
+  })
+  it('should get debug token', async () => {
+    await sut.loadUser({ token: 'any_client_token' })
+
+    expect(hhtpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/debug_token',
+      params: {
+        access_token: 'any_app_token',
+        input_token: 'any_client_token'
       }
     })
   })
