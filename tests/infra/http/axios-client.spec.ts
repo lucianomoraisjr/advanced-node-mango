9c125a381a9b69b3cf86a430fc40f93924560a2f
Luciano Junior <lucianomoraisjr@gmail.com>
2022-07-21 09:55:50 -0300

    islate SUT cration

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -9,22 +9,28 @@ class AxiosHttpClient {
 }
 
 describe('AxiosHttpClient', () => {
+  let sut: AxiosHttpClient
+  let fakeAxios: jest.Mocked<typeof axios>
+  let url: string
+  let params: object
+  beforeAll(() => {
+    url = 'any_url'
+    params = { any: 'any' }
+    fakeAxios = axios as jest.Mocked<typeof axios>
+  })
+
+  beforeEach(() => {
+    sut = new AxiosHttpClient()
+  })
   describe('get', () => {
     it('should call get with correct params ', async () => {
-      const fakeAxios = axios as jest.Mocked<typeof axios>
-      const sut = new AxiosHttpClient()
-
       await sut.get({
-        url: 'any_url',
-        params: {
-          any: 'any'
-        }
+        url,
+        params
       })
 
-      expect(fakeAxios.get).toHaveBeenCalledWith('any_url', {
-        params: {
-          any: 'any'
-        }
+      expect(fakeAxios.get).toHaveBeenCalledWith(url, {
+        params
       })
       expect(fakeAxios.get).toHaveBeenCalledTimes(1)
     })
