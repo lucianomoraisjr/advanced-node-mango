fd71534f285ad1b2a4cc8cbe65673a39a28025e6
Luciano Junior <lucianomoraisjr@gmail.com>
2022-07-27 09:47:09 -0300

    ensure PgUserAccountrepository returns undefined if email does not exists

--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -44,6 +44,21 @@ describe('PgUerAccountRepository', () => {
       const account = await sut.load({ email: 'existing_email' })
 
       expect(account).toEqual({ id: '1' })
+      await connection.close()
+    })
+    it('should return undefined if email does not exists', async () => {
+      const db = newDb()
+      const connection = await db.adapters.createTypeormConnection({
+        type: 'postgres',
+        entities: [PgUser]
+      })
+
+      await connection.synchronize()
+      const sut = new PgUserAccountrepository()
+      const account = await sut.load({ email: 'new_email' })
+
+      expect(account).toBeUndefined()
+      await connection.close()
     })
   })
 })
