e91a1865d8dd350b4e8a5a20a176633d97f64efd
Luciano Junior <lucianomoraisjr@gmail.com>
2022-07-27 16:44:05 -0300

    ensure FacebookLoginController returns 400 if token is emty

---
+++ tests/application/controllers/facebook-login.spec.ts
@@ -0,0 +1,24 @@
+
+class FacebookLoginController {
+  async handle (httpRequest: any): Promise<HttoResponse> {
+    return {
+      statusCode: 400,
+      data: new Error('The field token is required')
+    }
+  }
+}
+type HttoResponse = {
+  statusCode: number
+  data: any
+
+}
+describe('FacebookLoginController', () => {
+  it('should return 400 if emty', async () => {
+    const sut = new FacebookLoginController()
+    const httoResponse = await sut.handle({ token: '' })
+    expect(httoResponse).toEqual({
+      statusCode: 400,
+      data: new Error('The field token is required')
+    })
+  })
+})
