f63b4c7396cab4bd0d3540ccc9bcd425b4d4857a
Luciano Junior <lucianomoraisjr@gmail.com>
2022-08-04 09:41:46 -0300

    ensure ExpressRouter calls handle with correct request

--- src/main/routes/login.ts
+++ src/main/routes/login.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 
 export default (router: Router): void => {
-  router.post('/api', (req, res) => {
+  router.post('/api/login/facebook', (req, res) => {
     res.send({ data: 'any_data' })
   })
 }
---
+++ tests/infra/http/express-router.spec.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express'
+import { Controller } from '@/application/controllers'
+import { getMockReq, getMockRes } from '@jest-mock/express'
+import { mock } from 'jest-mock-extended'
+
+class ExpressRouter {
+  constructor (private readonly controller: Controller) {}
+  async adapt (req: Request, res: Response): Promise<void> {
+    await this.controller.handle({ ...req.body })
+  }
+}
+
+describe('ExpressRouter', () => {
+  it('shoud call handle with correct request', async () => {
+    const req = getMockReq({ body: { any: 'any' } })
+    const { res } = getMockRes()
+    const controller = mock<Controller>()
+    const sut = new ExpressRouter(controller)
+
+    await sut.adapt(req, res)
+
+    expect(controller.handle).toHaveBeenCalledWith({ any: 'any' })
+  })
+})
