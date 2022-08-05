e5302fd10fa5a9dc241e9927edd7c77fd9f37ae0
Luciano Junior <lucianomoraisjr@gmail.com>
2022-08-05 15:13:48 -0300

    connect to postgres db beforre server init

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,5 +1,10 @@
 import './config/module-alias'
-import 'reflect-metadata'
 import { app } from '@/main/config/app'
 import { env } from '@/main/config/env'
-app.listen(env.appPort, () => console.log(`Server running at http://localhost:${env.appPort}`))
+import { config } from '@/infra/postgres/helpers'
+import 'reflect-metadata'
+import { createConnection } from 'typeorm'
+
+createConnection(config)
+  .then(() => app.listen(env.appPort, () => console.log(`Server running at http://localhost:${env.appPort}`)))
+  .catch(console.error)
--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -13,7 +13,7 @@ describe('Facebook Api Integration Tests', () => {
     )
   })
   it('should return a Facebook User of token is valid', async () => {
-    const fbUser = await sut.loadUser({ token: 'EAAGf7IySvo8BAFGQLcQVhZAWSly8BgTZAto3RosiwjlEHUQUPJkZBmprRBm6ilqhzf0ADRahq5yWOfK8AnrbZA8ZArfhwqOaJZAUsyHwsbBEo32zO032qEz1pWXxhsL2X8R1nsPPvcabC3UhcLEQX3TPxbRR06ZAc1LzYTDjfikbAPvyv7IsusdkVK8nneKpHZBD7MT6HdZBDk3svlOkovvVf' })
+    const fbUser = await sut.loadUser({ token: 'EAAGf7IySvo8BAL6lvVPt8uXwXrMwwGVUZBvJ0NQET9QW9UKJwP2SZCyzKKHOh1GZCWCNK9YxlggAo23X4g1eiDW8aW1SoFHk1kc9U8v2P2SPW1jOIn86kWX8KKYNFKO94EpsWLUDq7RGiYmgOfi4UhI1RHyJtI3I67MiiXaM77RuvnnAy3SaLA4G7hMdGZCoZCWS9HJwt4gZDZD' })
     expect(fbUser).toEqual({
       facebookId: '109548355186347',
       email: 'mango_fzgkqeh_teste@tfbnw.net',
