96c6a7e721834e8718a6d012a1feadaa9c78876b
Luciano Junior <lucianomoraisjr@gmail.com>
2022-08-08 17:16:32 -0300

    ensure Authorize returns accessToken

---
+++ tests/domain/use-cases/authorize.spec.ts
@@ -0,0 +1,40 @@
+import { mock, MockProxy } from 'jest-mock-extended'
+interface TokenValidator {
+  validateToken: (params: TokenValidator.Params) => Promise<TokenValidator.Result>
+}
+export namespace TokenValidator {
+  export type Params = { token: string}
+  export type Result = string
+}
+
+type Setup = (crypto: TokenValidator) => Authorize
+type Input = {token: string}
+type Output = string
+type Authorize = (params: Input) => Promise<Output>
+
+const setupAuthorize: Setup = crypto => async params => {
+  return await crypto.validateToken(params)
+}
+describe('FacebookAuthentication', () => {
+  let crypto: MockProxy<TokenValidator>
+  let sut: Authorize
+  let token: string
+  beforeAll(() => {
+    token = 'any_token'
+    crypto = mock()
+    crypto.validateToken.mockResolvedValue('any_value')
+  })
+  beforeEach(() => {
+    sut = setupAuthorize(crypto)
+  })
+
+  it('should call TokenValidation with correct params', async () => {
+    await sut({ token })
+    expect(crypto.validateToken).toHaveBeenCalledWith({ token })
+    expect(crypto.validateToken).toHaveBeenCalledTimes(1)
+  })
+  it('should return token', async () => {
+    const userId = await sut({ token })
+    expect(userId).toBe('any_value')
+  })
+})
