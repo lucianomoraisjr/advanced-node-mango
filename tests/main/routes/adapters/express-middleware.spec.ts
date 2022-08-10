74d3bb2d0e7e76c053020426acd9460482b0f23e
Luciano Junior <lucianomoraisjr@gmail.com>
2022-08-10 10:06:47 -0300

    ensure ExpressMidleware add data to req.locals

--- tests/main/routes/adapters/express-middleware.spec.ts
+++ tests/main/routes/adapters/express-middleware.spec.ts
@@ -1,17 +1,25 @@
 import { HttpResponse } from '@/application/helpers'
 import { getMockReq, getMockRes } from '@jest-mock/express'
-import { RequestHandler, Request, NextFunction, Response } from 'express'
+import { NextFunction, Request, Response, RequestHandler } from 'express'
 import { mock, MockProxy } from 'jest-mock-extended'
 
-type Adapeter = (middleware: Middleware) => RequestHandler
+type Adapter = (middleware: Middleware) => RequestHandler
 
-const adaptExpressMidleware: Adapeter = middleware => async (req, res, next) => {
+const adaptExpressMidleware: Adapter = middleware => async (req, res, next) => {
   const { statusCode, data } = await middleware.handle({ ...req.headers })
-  res.status(statusCode).json(data)
+  if (statusCode === 200) {
+    const entries = Object.entries(data).filter(entry => entry[1])
+    req.locals = { ...req.locals, ...Object.fromEntries(entries) }
+    next()
+  } else {
+    res.status(statusCode).json(data)
+  }
 }
-interface Middleware{
+
+interface Middleware {
   handle: (httpRequest: any) => Promise<HttpResponse>
 }
+
 describe('ExpressMidleware', () => {
   let req: Request
   let res: Response
@@ -25,39 +33,54 @@ describe('ExpressMidleware', () => {
     next = getMockRes().next
     middleware = mock<Middleware>()
     middleware.handle.mockResolvedValue({
-      statusCode: 500,
-      data: { error: 'any_error' }
+      statusCode: 200,
+      data: {
+        emptyProp: '',
+        nullProp: null,
+        undefinedProp: undefined,
+        prop: 'any_value'
+      }
     })
   })
+
   beforeEach(() => {
     sut = adaptExpressMidleware(middleware)
   })
+
   it('should call handle with correct request', async () => {
     await sut(req, res, next)
 
     expect(middleware.handle).toHaveBeenCalledWith({ any: 'any' })
     expect(middleware.handle).toHaveBeenCalledTimes(1)
   })
+
   it('should call handle with empty request', async () => {
     req = getMockReq()
+
     await sut(req, res, next)
 
     expect(middleware.handle).toHaveBeenCalledWith({})
     expect(middleware.handle).toHaveBeenCalledTimes(1)
   })
-  it('should call handle with empty request', async () => {
-    req = getMockReq()
-    await sut(req, res, next)
 
-    expect(middleware.handle).toHaveBeenCalledWith({})
-    expect(middleware.handle).toHaveBeenCalledTimes(1)
-  })
-  it('should call handle with empty request', async () => {
+  it('should respond with correct error and statusCode', async () => {
+    middleware.handle.mockResolvedValueOnce({
+      statusCode: 500,
+      data: { error: 'any_error' }
+    })
+
     await sut(req, res, next)
 
     expect(res.status).toHaveBeenCalledWith(500)
     expect(res.status).toHaveBeenCalledTimes(1)
     expect(res.json).toHaveBeenCalledWith({ error: 'any_error' })
     expect(res.json).toHaveBeenCalledTimes(1)
+  })
+
+  it('should add valid data to req.locals', async () => {
+    await sut(req, res, next)
+
+    expect(req.locals).toEqual({ prop: 'any_value' })
+    expect(next).toHaveBeenCalledTimes(1)
   })
 })
