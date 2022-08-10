69a679678e3939befa97c04026f804d46118667c
Luciano Junior <lucianomoraisjr@gmail.com>
2022-08-10 08:18:20 -0300

    improve code quality

--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -1,19 +1,25 @@
-import { forbidden, HttpResponse } from '@/application/helpers'
+import { forbidden, HttpResponse, ok } from '@/application/helpers'
 import { ForbiddenError } from '@/application/errors'
 import { RequiredStringValidator } from '@/application/validation'
 import { Authorize } from '@/domain/use-cases'
 
 type HttpRequest = { authorization: string}
+type Model = Error | {userId: string}
 class AuthenticationMiddleware {
   constructor (private readonly authorize: Authorize) {}
-  async handle ({ authorization }: HttpRequest): Promise<HttpResponse<Error> | undefined> {
-    const error = new RequiredStringValidator(authorization, 'authorization').validate()
-    if (error !== undefined) return forbidden()
+  async handle ({ authorization }: HttpRequest): Promise<HttpResponse<Model>> {
+    if (!this.validate({ authorization })) return forbidden()
     try {
-      await this.authorize({ token: authorization })
+      const userId = await this.authorize({ token: authorization })
+      return ok({ userId })
     } catch {
       return forbidden()
     }
+  }
+
+  private validate ({ authorization }: HttpRequest): boolean {
+    const error = new RequiredStringValidator(authorization, 'authorization').validate()
+    return error === undefined
   }
 }
 describe(' AuthenticationMiddleware', () => {
@@ -22,7 +28,7 @@ describe(' AuthenticationMiddleware', () => {
   let authorize: jest.Mock
   beforeAll(() => {
     authorization = 'any_authorization_token'
-    authorize = jest.fn()
+    authorize = jest.fn().mockResolvedValue('any_user_Id')
   })
 
   beforeEach(() => {
@@ -65,6 +71,14 @@ describe(' AuthenticationMiddleware', () => {
     expect(httpResponse).toEqual({
       statusCode: 403,
       data: new ForbiddenError()
+    })
+  })
+  it('should return 200 with userId on success', async () => {
+    const httpResponse = await sut.handle({ authorization })
+
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      data: { userId: 'any_user_Id' }
     })
   })
 })
